012b9e7f27642367cd0c2a64bb9f295e6771750b
Yann Offredi <yann.offredi@wheel.digital>
2021-10-04 22:09:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import AlbumList from './components/AlbumList/AlbumList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AlbumList />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload test.
         </p>
         <a
           className="App-link"
---
+++ src/components/AlbumList/AlbumList.js
@@ -0,0 +1,7 @@
+function AlbumList() {
+  return (
+    <h1>TEST</h1>
+  );
+}
+
+export default AlbumList;
